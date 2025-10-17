fd5554c38b58008c5216b9227af97f98d9e1c5ad
Kenyanengine <mashalaghungala@gmail.com>
2025-10-17 22:49:43 +0300

    Create SmsReceiver.java

---
+++ SmsReceiver.java
@@ -0,0 +1,31 @@
+package com.bingwa.autoagent;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.telephony.SmsMessage;
+import android.widget.Toast;
+import android.util.Log;
+
+public class SmsReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Bundle bundle = intent.getExtras();
+        if (bundle != null) {
+            Object[] pdus = (Object[]) bundle.get("pdus");
+            if (pdus != null) {
+                for (Object pdu : pdus) {
+                    SmsMessage sms = SmsMessage.createFromPdu((byte[]) pdu);
+                    String sender = sms.getDisplayOriginatingAddress();
+                    String message = sms.getMessageBody();
+
+                    if (sender.contains("M-PESA")) {
+                        Toast.makeText(context, "M-PESA detected", Toast.LENGTH_SHORT).show();
+                        Log.d("BINGWA", "Message: " + message);
+                    }
+                }
+            }
+        }
+    }
+}
